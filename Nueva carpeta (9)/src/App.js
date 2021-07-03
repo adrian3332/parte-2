a47016a065de2b51cc2221627caa164b2912164b
adrian3332 <74941032+adrian3332@users.noreply.github.com>
2021-07-03 18:50:36 -0500

    Add files via upload

---
+++ Nueva carpeta (9)/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from '../src/components/navbar/navbar'
+import ListCards from '../src/components/cards/ListCards'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <ListCards></ListCards>
+    </div>
+  );
+}
+
+export default App;
